d372d69a0abb86a43a5785994012146884183278
harrism178996 <52997912+harrism178996@users.noreply.github.com>
2023-07-19 14:33:08 -0400

    Update Q.py

--- Q.py
+++ Q.py
@@ -18,3 +18,14 @@ time.sleep(1)
 led.value(1)
 time.sleep(1)
 led.value(0)
+
+button = Pin(0, Pin.IN)
+
+
+if button() == 0:
+    print('boot button pressed')
+    ugit.pull_all()
+    print('pulling github repo')
+else:
+        none
+    
