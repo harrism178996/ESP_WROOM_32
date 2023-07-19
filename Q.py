78d89d49a30852422f163aeca020ff2ce06a6db2
harrism178996 <52997912+harrism178996@users.noreply.github.com>
2023-07-19 15:27:45 -0400

    Update Q.py

--- Q.py
+++ Q.py
@@ -22,9 +22,19 @@ led.value(0)
 button = Pin(0, Pin.IN)
 
 
-if button() == 0:
-    print('boot button pressed')
-    ugit.pull_all()
-    print('pulling github repo')
+#if button() == 0:
+#   print('boot button pressed')
+#    ugit.pull_all()
+#    print('pulling github repo')
+    
 
+button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
+while True:
+    first = button.value()
+    time.sleep(0.01)
+    second = button.value()
+    if first and not second:
+        print('Button pressed!')
+    elif not first and second:
+        print('Button released!')
     
