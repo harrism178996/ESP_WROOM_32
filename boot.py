003fc8a9f5e5fee5d0bb8e31aaad449ff2808b24
harrism178996 <52997912+harrism178996@users.noreply.github.com>
2023-07-19 13:38:03 -0400

    Update boot.py
    import Q

--- boot.py
+++ boot.py
@@ -3,13 +3,8 @@
 
 import machine
 import time
+import Q
 
-print('hello ugit users!')
-
-print('I am working on an issue with ugit running into mem errors when connected to a computer')
-print('ugit seems to work well when battery powered or not in an active REPL session.')
-
-print('I hope you enjoy it!')
-    
+print('Boot initialized')
 
 
